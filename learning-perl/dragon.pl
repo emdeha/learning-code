650e909256bed24fc8187f7416555334fef96191
emdeha <cpi.cecko@gmail.com>
2015-03-06 18:47:00 +0200

    add coords

--- learning-perl/dragon.pl
+++ learning-perl/dragon.pl
@@ -6,14 +6,15 @@ use warnings;
 
 use Getopt::Std;
 
-sub usage($) {
+sub usage($) 
+{
     my ($err) = @_;
 
     my $s = 
 <<USAGE
 Usage: dragon [-m mode] <generation>
 
-    -m    the display mode (turns, turtle)
+    -m    the display mode; use -m list to show available modes
 The generation must be a positive integer.
 USAGE
     ;
@@ -21,8 +22,46 @@ USAGE
     $err? die($s): print $s;
 }
 
-sub version() {
+sub version() 
+{
     say 'dragon 0.01';
+}
+
+sub step($ $ $)
+{
+    my ($x, $y, $dir) = @_;
+
+    my @steps = ( (0, 1), (1, 0), (0, -1), (-1, 0) );
+    my ($dx, $dy) = @steps[2 * $dir, 2 * $dir + 1];
+
+    return ($x + $dx, $y + $dy);
+}
+
+sub turn($ $)
+{
+    my ($dir, $turn) = @_;
+
+    return ($dir - 1 + 2 * $turn) % 4;
+}
+
+sub get_coords(@)
+{
+    my (@turns) = @_;
+    my ($x, $y) = (0, 0);
+    my $dir = 0;
+    my @coords;
+
+    push @coords, ($x, $y);
+    ($x, $y) = step $x, $y, $dir;
+    push @coords, ($x, $y);
+
+    for my $turn (@turns) {
+        $dir = turn $dir, $turn;
+        ($x, $y) = step $x, $y, $dir;
+        push @coords, ($x, $y);
+    }
+
+    return @coords;
 }
 
 MAIN:
@@ -36,6 +75,7 @@ MAIN:
     my $mode = 'turns';
     if (exists($opts{m})) {
         my %modes = (
+            'coords' => 'Coordinates',
             'turns' => 'The sequence as turns',
             'turtle' => 'Turtle graphics'
         );
@@ -59,11 +99,23 @@ MAIN:
 
     if ($mode eq 'turns') {
         say @turns;
+        exit(0);
     } elsif ($mode eq 'turtle') {
         say 'forward 10';
         for my $turn (@turns) {
             say $turn? 'right': 'left', ' 90';
             say 'forward 10';
         }
+        exit(0);
+    }
+
+    my @coords = get_coords @turns;
+    if ($mode eq 'coords') {
+        my @c;
+        while (@coords) {
+            my ($x, $y) = (shift @coords, shift @coords);
+            push @c, "($x, $y)"
+        }
+        say "@c";
     }
 }
--- learning-perl/t/01-valid.t
+++ learning-perl/t/01-valid.t
@@ -4,11 +4,16 @@ use 5.012;
 use strict;
 use warnings;
 
-use Test::More tests => 4;
+use Test::More tests => 5;
 use Test::Command;
 
+# Raw
 stdout_is_eq('perl dragon.pl 1', "1\n");
 stdout_is_eq('perl dragon.pl 4', "110110011100100\n");
 
+# Turns
 stdout_is_eq('perl dragon.pl -m turns 1', "1\n");
 stdout_is_eq('perl dragon.pl -m turns 4', "110110011100100\n");
+
+# Coords
+stdout_is_eq('perl dragon.pl -m coords 2', "(0, 0) (0, 1) (1, 1) (1, 0) (2, 0)\n");
