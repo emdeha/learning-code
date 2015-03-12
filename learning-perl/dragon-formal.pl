c12e989e1fd3a8168f291541d33d1641d39ca745
emdeha <cpi.cecko@gmail.com>
2015-03-12 16:40:18 +0200

    implements generic drawer of dragon and sierpinski

--- learning-perl/dragon-formal.pl
+++ learning-perl/dragon-formal.pl
@@ -4,85 +4,89 @@ use 5.012;
 use strict;
 use warnings;
 
+use Getopt::Std;
 use GD::Simple;
 use List::Util 'max';
 use Math::Trig ':pi';
 
 # Dragon curve
-# my $init = "FX";
-# my %rules = ( 
-#     X => "X+YF+",
-#     Y => "-FX-Y"
-# );
-# 
-# sub getCoords($)
-# {
-#     my $grad = $_[0];
-#     my $expand = $init;
-#     while ($grad-- >= 1) {
-#         $expand =~ s/(X|Y)/$rules{$1}/g;
-#     }
-# 
-#     my @coords;
-#     my ($x, $y) = (0, 0);
-#     my $theta = 0;
-#     for (split //, $expand) {
-#         if (/F/) {
-#             $x += 2 * cos($theta);
-#             $y += 2 * sin($theta);
-#             push @coords, ($x, $y);
-#         } elsif (/\+/) {
-#             $theta += pi / 2;
-#         } elsif (/\-/) {
-#             $theta -= pi / 2;
-#         }
-#     }
-# 
-#     return @coords;
-# }
+my $dInit = "FX";
+my $dAngle = pi / 2;
+my $dRight = qr/\+/;
+my $dLeft = qr/\-/;
+my $dFwd = qr/F/;
+my $dExp = qr/(X|Y)/;
+my %dRules = ( 
+    X => "X+YF+",
+    Y => "-FX-Y"
+);
 
 # Sierpinski triangle
-my $init = "F-G-G";
-my %rules = (
+my $sInit = "F-G-G";
+my $sAngle = 2 * pi / 3;
+my $sRight = qr/\-/;
+my $sLeft = qr/\+/;
+my $sFwd = qr/F|G/;
+my $sExp = qr/(G|F)/;
+my %sRules = (
     F => "F-G+F+G-F",
     G => "GG"
 );
 
-sub getCoords($)
+sub getCoords($ $)
 {
-     my $grad = $_[0];
+     my ($grad, $figure) = @_;
+
+     my $left = $dLeft;
+     my $right = $dRight;
+     my $fwd = $dFwd;
+     my $angle = $dAngle;
+     my $init = $dInit;
+     my %rules = %dRules;
+     my $exp = $dExp;
+     if ($figure eq 'sierp') {
+         $left = $sLeft;
+         $right = $sRight;
+         $fwd = $sFwd;
+         $angle = $sAngle;
+         $init = $sInit;
+         %rules = %sRules;
+         $exp = $sExp;
+     }
+
      my $expand = $init;
      while ($grad-- >= 1) {
-         $expand =~ s/(G|F)/$rules{$1}/g;
+         $expand =~ s/$exp/$rules{$1}/g;
      }
 
      my @coords;
      my ($x, $y) = (0, 0);
      my $theta = 0;
      for (split //, $expand) {
-         if (/F|G/) {
+         if (/$fwd/) {
              $x += 2 * cos($theta);
              $y += 2 * sin($theta);
              push @coords, ($x, $y);
-         } elsif (/\+/) {
-             $theta -= 2 * pi / 3;
-         } elsif (/\-/) {
-             $theta += 2 * pi / 3;
+         } elsif (/$right/) {
+             $theta += $angle;
+         } elsif (/$left/) {
+             $theta -= $angle;
          }
      }
- 
+
      return @coords;
 }
 
-MAIN:
+sub draw(@)
 {
+    my @coords = @_;
+
     my $img = GD::Simple->new(600, 600);
     $img->bgcolor('white');
     $img->fgcolor('black');
 
     $img->moveTo(300, 300);
 
-    my @coords = getCoords(10);
     my $maxval = max map { abs($_) } @coords;
     my $scale = 256 / $maxval;
     while (@coords) {
@@ -95,4 +99,55 @@ MAIN:
     binmode STDOUT;
     print $img->png;
     exit(0);
+}
+
+sub version()
+{
+    say 'Dragon Formal v0.1'
+}
+
+sub usage($_)
+{
+    my ($err) = @_;
+
+    my $s = <<USAGE
+Usage: dragon-formal [-f figure] <generation>
+
+    -f     the figure which you want to draw; use -f list to show available figures
+The generation must be positive integer
+USAGE
+    ;
+
+    $err? die($s): print $s;
+}
+
+MAIN:
+{
+    my %opts;
+    getopts('hf:v', \%opts) or usage(1);
+    version() if $opts{v};
+    usage(0) if $opts{h};
+    exit(0) if $opts{h} || $opts{v};
+
+    my $figure = 'dragon';
+    if (exists($opts{f})) {
+        my %figures = (
+            'dragon' => "Draw Dragon curve",
+            'sierp' => "Draw Sierpinski triange",
+        );
+
+        if ($opts{f} eq 'list') {
+            say 'Available figures:';
+            say "$_\t$figures{$_}" for sort keys %figures;
+            exit(0);
+        }
+        usage(1) unless exists $figures{$opts{f}};
+        $figure = $opts{f};
+    }
+
+    usage(1) unless @ARGV == 1 && $ARGV[0] =~ /^[1-9][0-9]*$/;
+    my $generation = $ARGV[0];
+
+    my @coords = getCoords($generation, $figure);
+    draw(@coords);
 }
