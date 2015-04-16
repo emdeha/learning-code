8d7cc426488276319faf38593939601360483a2a
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-04-16 12:04:59 +0300

    implements two fibonacci functions

---
+++ learning-haskell/yorgey/hw6/Fibonacci.hs
@@ -0,0 +1,13 @@
+module Fibonacci where
+
+
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = (fib (n-1)) + (fib (n-2))
+
+fibs1 :: [Integer]
+fibs1 = map fib [0..]
+
+fibs2 :: [Integer]
+fibs2 = 0 : 1 : zipWith (+) (tail fibs2) (init fibs2)
