7fea1eea68d4f431e9a0f84804b89b7d020d5280
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-05-01 17:38:43 +0300

    some functors

--- learning-haskell/yorgey/typeclassopedia/FunctorEx.hs
+++ learning-haskell/yorgey/typeclassopedia/FunctorEx.hs
@@ -1,4 +1,6 @@
+{-# OPTIONS_GHC -Wall #-}
 module FunctorEx where
+
 
 data MyEither e x = L e
                   | R x
@@ -8,3 +10,24 @@ data MyEither e x = L e
 instance Functor (MyEither e) where
     fmap _ (L e) = L e
     fmap g (R x) = R (g x)
+
+
+data Pair a = Pair a a
+    deriving (Show)
+
+instance Functor Pair where
+    fmap g (Pair a b) = Pair (g a) (g b)
+
+
+data ITree a = Leaf (Int -> a)
+             | Node [ITree a]
+
+instance Functor ITree where
+    fmap g (Leaf f)      = Leaf $ g . f
+    fmap g (Node forest) = Node $ map (fmap g) forest
+
+f :: Int -> Char
+f = undefined
+
+x :: a
+x = undefined
