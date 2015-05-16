fd8b08bba4cc3349f6995f2384f50dcb60de7cdb
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-05-16 11:28:57 +0300

    implements distProbs

---
+++ learning-r/Homework/CLT/distProbs.R
@@ -0,0 +1,22 @@
+geomProb <- function() {
+    sum (dgeom(4:12, 0.123))
+}
+
+poisProb <- function() {
+    sum (dpois(7:17, 2))
+}
+
+normQuant <- function() {
+    x = qnorm(0.78, mean=10, sd=2)
+    c(-x,x)
+}
+
+tQuant <- function() {
+    x = qt(0.44, df=4)
+    c(-x,x)
+}
+
+chisqQuant <- function() {
+    x = qchisq(0.55 + pchisq(2, df=5), df=5)
+    x
+}
