783f20c147d3792438c8a28cff9b92386b210099
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-07-15 09:45:46 +0300

    Generates the simple random case

--- learning-algo/randoms/random.cpp
+++ learning-algo/randoms/random.cpp
@@ -1,23 +1,58 @@
 #include <iostream>
 #include <vector>
-#include <stdlib.h>
-#include <time.h>
 #include <sstream>
+#include <random>
+#include <iomanip>
+#include <map>
 
 
-int random(int a, int b)
+int RandomZeroOne()
 {
-    return b;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+
+    std::bernoulli_distribution d(0.5);
+
+    return d(gen);
+}
+
+int RandomRange(int a, int b)
+{
+    int x = RandomZeroOne();
+    int y = RandomZeroOne();
+
+    if (x && !y)
+    {
+        return a;
+    }
+    if (!x && y)
+    {
+        return b;
+    }
+    if (x && y)
+    {
+        return (a + b) / 4;
+    }
+
+    return (a + b) / 1.5;
 }
 
 int main(int argc, char *argv[]) 
 {
-    srand(time(0));
-
     int a,b;
     std::stringstream(argv[1]) >> a;
     std::stringstream(argv[2]) >> b;
-    std::cout << random(a,b) << std::endl;
+
+    std::map<int, int> hist;
+    for (int i = 0; i < 10000; ++i) 
+    {
+        ++hist[RandomRange(a, b)];
+    }
+    for (auto p : hist)
+    {
+        std::cout << std::setw(5) << p.first
+                  << ' ' << std::string(p.second/500, '*') << '\n';
+    }
 
     return 0;
 }
