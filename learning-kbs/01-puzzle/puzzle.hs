87cf2bdc57584d9b04996f9c3b401fbf31423ff4
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-10-12 13:53:58 +0300

    Implements DFS algorithm.

--- learning-kbs/01-puzzle/puzzle.hs
+++ learning-kbs/01-puzzle/puzzle.hs
@@ -2,23 +2,33 @@ import System.IO
 import Data.Matrix
 
 
-type Input = String
+type Input = Char
 type Element = Char
-type Node = Matrix Element
-
-data Tree = State Node [Tree]
-          | Nil
-         deriving Show
-type PuzzleStates = Tree
+type Node = (Int, Char)
 
 
-inputToStates :: Input -> PuzzleStates
-inputToStates inp = State (fromList 3 3 inp) []
+tailOrNil [] = []
+tailOrNil (x:xs) = xs
 
-generateChildren :: PuzzleStates -> PuzzleStates
-generateChildren (State nd _) = undefined
+inputToStates :: Input -> Node
+inputToStates inp = (1, inp)
+
+generateChildren :: Node -> [Node]
+generateChildren (i,c) = [(succ i, succ c),
+                          (succ i, succ $ succ $ succ c)]
+
+findNode :: Node -> Node -> [Node] -> [Node] -> [Node] -> [Node]
+findNode _     _   _    visited []     = reverse visited
+findNode start end path visited (x:xs)
+    | snd x == snd end = reverse (x:path)
+    | elem x visited =
+        findNode x end (tailOrNil path) visited xs
+    | otherwise      =
+        findNode x end (start:path) (x:visited) ((generateChildren start) ++ xs)
 
 
 main :: IO ()
 main = do
-  putStrLn "Hello!"
+    let start = inputToStates 'A'
+        end = (4, 'J')
+    putStrLn $ show $ findNode start end [] [] [start]
