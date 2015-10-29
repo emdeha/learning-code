c20c9df2802581c7e41f6de4d3f61ade68833d6f
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-10-29 20:43:07 +0200

    Fixes bug with BPQueueMax `maxView`.
    maxView expected the max element to be found in the array with the
    highest priority. This isn't the case though, because we could have an
    empty array there. In this case, we must go one priority down and search
    again.

--- learning-kbs/01-puzzle/Data/BPQueueMax.hs
+++ learning-kbs/01-puzzle/Data/BPQueueMax.hs
@@ -18,19 +18,24 @@ insert k a (BPQueueMax arr) =
     let (f, l) = bounds arr 
     in  if f <= k && k <= l
         then BPQueueMax $ arr // [(k, (arr ! k) S.|> a)]
-        else BPQueueMax $ updateIndices arr f k l a 
-  where updateIndices :: Array Int (S.Seq a) -> Int -> Int -> Int -> a -> Array Int (S.Seq a)
-        updateIndices arr' f k' l a'
-            | k' > l     = 
-                array (f, k') (assocs arr' ++ (k', S.singleton a') : (take (k'-l) (enumBPQ (l, S.empty))))
-            | otherwise =
-                error $ "Priority out of bounds (" ++ (show f) ++ "," ++ (show l) ++ ")"
+        else BPQueueMax $ addPriority arr f l k a 
+
+addPriority :: Array Int (S.Seq a) -> Int -> Int -> Int -> a -> Array Int (S.Seq a)
+addPriority arr f l k a
+    | k > l =
+        let old = assocs arr
+            new = (k, S.singleton a) : (take (k-l) $ enumBPQ (l, S.empty))
+        in  array (f, k) (old ++ new)
+    | otherwise =
+        error $ "Priority out of bounds (" ++ (show f) ++ "," ++ (show l) ++ ")"
 
 
 singleton :: Int -> a -> BPQueueMax a
 singleton k a
-    | k >= 0    =
-        BPQueueMax (array (0, k) ((k, S.singleton a) : (take k (enumBPQ (0, S.empty)))))
+    | k >= 0 =
+        let first = (k, S.singleton a)
+            rest = take k $ enumBPQ (0, S.empty)
+        in  BPQueueMax (array (0, k) (first : rest))
     | otherwise =
         error "Priority must be positive"
 
@@ -40,11 +45,15 @@ enumBPQ bpq@(a, que) = bpq : enumBPQ (succ a, que)
 
 maxView :: BPQueueMax a -> Maybe (a, BPQueueMax a)
 maxView (BPQueueMax arr) =
-    let (_, maxIx) = bounds arr
-        que = arr ! maxIx
-    in  if S.null que
-        then Nothing
-        else detachHeadAt (S.viewl que) maxIx arr
-  where detachHeadAt :: S.ViewL a -> Int -> Array Int (S.Seq a) -> Maybe (a, BPQueueMax a)
-        detachHeadAt S.EmptyL    _ _   = Nothing
+    extractAt (snd . bounds $ arr) arr
+  where extractAt :: Int -> Array Int (S.Seq a) -> Maybe (a, BPQueueMax a)
+        extractAt 0  _    = Nothing
+        extractAt ix arr' =
+            let que = arr' ! ix
+            in  if S.null que
+                then extractAt (ix-1) arr'
+                else detachHeadAt (S.viewl que) ix arr'
+
+        detachHeadAt :: S.ViewL a -> Int -> Array Int (S.Seq a) -> Maybe (a, BPQueueMax a)
+        detachHeadAt S.EmptyL    _ _    = Nothing
         detachHeadAt (x S.:< xs) k arr' = Just (x, BPQueueMax (arr' // [(k, xs)]))
