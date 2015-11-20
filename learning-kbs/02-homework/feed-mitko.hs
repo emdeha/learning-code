a8bb995d9a6824f32ff70d44bc2c429949212de1
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-11-20 09:25:07 +0200

    The algorithm works!

--- learning-kbs/02-homework/feed-mitko.hs
+++ learning-kbs/02-homework/feed-mitko.hs
@@ -14,8 +14,9 @@ import System.Environment (getArgs)
 
 import qualified Data.Vector as V
 import qualified Data.ByteString.Lazy as BS
-import qualified Data.HashSet as HS
-import qualified Data.PQueue.Prio.Max as PS
+import qualified Data.Map as M
+import qualified Data.PQueue.Prio.Min as PS
+import Data.Maybe (catMaybes)
 import Data.List
 import Data.Csv
 
@@ -30,6 +31,7 @@ import Data.Csv
   4. Construction from a CSV file based on some simple rules
 -}
 data Tile = Empty | Wall | Water
+          deriving (Eq)
 
 instance Show Tile where
   show Empty = " "
@@ -59,6 +61,33 @@ loadLabyrinth labData =
       let rows = V.map toVectorTiles rawLab
       in  Right $ Labyrinth rows
 
+getAtPosition :: Labyrinth -> Point -> Maybe (Tile, Point)
+getAtPosition (Labyrinth lab) (x,y) =
+-- TODO: Simplify
+  case lab V.!? y of
+    Just row ->
+      case row V.!? x of
+        Just tile -> Just (tile, (x, y))
+        Nothing   -> Nothing
+    Nothing -> Nothing
+  
+getAtPositions :: Labyrinth -> [Point] -> [Maybe (Tile, Point)]
+getAtPositions lab = map (getAtPosition lab)
+
+children :: Labyrinth -> Point -> [(Tile, Point)]
+children lab (x, y) = 
+  catMaybes . getAtPositions lab $ [(x-1,y),(x,y-1),(x+1,y),(x,y+1),
+                                    (x-1,y-1),(x-1,y+1),(x+1,y+1),(x+1,y+1)]
+
+distance :: Point -> Point -> Int
+distance (x1, y1) (x2, y2) = (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1)
+
+calcPrio :: (Tile, Point) -> Int
+calcPrio child = 
+  case fst child of
+    Water -> 2
+    Empty -> 1
+    Wall -> 999
 
 type Point = (Int, Int)
 type Path = [Point]
@@ -70,37 +99,69 @@ findPath labData start end =
     Right lab -> Right $ solve lab start end
 
 solve :: Labyrinth -> Point -> Point -> Path
-solve lab start end = 
-  aStarSearch lab end [] HS.empty (uncurry PS.singleton . calcPrio $ start end)
+solve lab start end =
+  let came_from = M.singleton start Nothing 
+      cost_so_far = M.singleton start 0
+  in  aStarSearch lab end came_from cost_so_far (PS.singleton 0 start)
+
+
+data NextChild = NextChild 
+    { position :: Point
+    , prio :: Int
+    , cost :: Int
+    }
+
+getNextChildPrio :: NextChild -> (Int, Point)
+getNextChildPrio nc = (prio nc, position nc)
+
+getNextChildCost :: NextChild -> (Point, Int)
+getNextChildCost nc = (position nc, cost nc)
 
 aStarSearch :: 
-    Labyrinth -> 
-    Point -> 
-    [Int] ->
-    HS.HashSet Point -> 
-    PS.MaxPQueue Int Point ->
+    Labyrinth ->
+    Point ->
+    M.Map Point (Maybe Point) ->
+    M.Map Point Int ->
+    PS.MinPQueue Int Point ->
     Path
-aStarSearch lab end gScores visited pq =
-  let que = PS.maxView pq
+aStarSearch lab end came_from cost_so_far pq =
+  let que = PS.minView pq
   in  case que of
-      Nothing -> []
-      Just (x, xs) -> go x xs
-  where go :: Point -> PS.MaxPQueue Int Point -> Path
+        Nothing      -> []
+        Just (x, xs) -> go x xs
+  where go :: Point -> PS.MinPQueue Int Point -> Path
         go x xs
-          | x == end            = reverse $ constructPathFromEnd x
-          | HS.member x visited = aStarSearch lab end gScores visited xs
-          | otherwise =
-              let visited' = HS.insert x visited
-                  front f = 
-                    foldl (flip . uncurry $ PS.insert) xs 
-                          (map (toScore ((+) find (==x) gScores) . calcPrio) (getNeighbors lab x))
-              -- TODO: Update gScores
-              in  aStarSearch lab end gScores visited' front
+          | x == end  = x : constructPath came_from x
+          | otherwise = 
+              let next = foldr calcNext [] (children lab x)
+                  cost_so_far' = foldr (\chld acc -> 
+                    M.insert (position chld) (cost chld) acc) cost_so_far next
+                  came_from' = foldr (\chld acc ->
+                    M.insert (position chld) (Just x) acc) came_from next
+                  front = foldr ((uncurry PS.insert) . getNextChildPrio) xs next
+              in  aStarSearch lab end came_from' cost_so_far' front
+              where calcNext child acc =
+                      let new_cost = cost_so_far M.! x + distance x (snd child)
+                      in  if (fst child /= Wall) && 
+                             (not (M.member (snd child) came_from) || 
+                             (new_cost < cost_so_far M.! (snd child)))
+                          then NextChild { position = (snd child)
+                                         , prio = new_cost + calcPrio child
+                                         , cost = new_cost
+                                         } : acc
+                          else acc
+
+constructPath :: M.Map Point (Maybe Point) -> Point -> Path
+constructPath paths current = --error . show $ paths
+  case M.lookup current paths of
+    Just (Just parent) ->
+      parent : constructPath (M.delete current paths) parent
+    _ -> []
 
 
 pathToString :: Either String Path -> String
 pathToString (Left err) = err
-pathToString (Right path) = intersperse ',' . show $ path
+pathToString (Right path) = show $ reverse path
 
 main :: IO ()
 main = do
