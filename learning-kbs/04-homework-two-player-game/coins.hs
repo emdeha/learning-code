182b6bf09d2844dce2f64ecb569156607d6e4863
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-12-27 12:06:22 +0200

    Implements child generation for the coins problem.

--- learning-kbs/04-homework-two-player-game/coins.hs
+++ learning-kbs/04-homework-two-player-game/coins.hs
@@ -15,37 +15,86 @@ data Turn = Me | Opponent
 
 data Node = Node
           { parent :: Node
-          , board :: Int
+            -- TODO: Add starting index to board
+          , board :: [Bool]
           , turn :: Turn
           }
           | Nil
     deriving Show
 
-generateChild :: Node -> Int -> Node
-generateChild p n = Node { parent = p, board = n, turn = neg . turn $ p }
-  where
+makeChild :: Node -> [Bool] -> Node
+makeChild parent board =
+    Node { parent = parent, board = board, turn = neg . turn $ parent }
+  where 
     neg Me = Opponent
     neg Opponent = Me
 
-evaluate :: Node -> Int
-evaluate nd = undefined
-
 getChildren :: Node -> [Node]
-getChildren nd =
-  let coins_count = board nd
-  in  map (generateChild nd) (takeWhile (>=0) $ iterate (flip (-) 1) (coins_count - 1))
+getChildren p =
+    let boards = takeCoins $ board p
+    in  map (makeChild p) boards
+  where
+    takeCoins board =
+      let boardLen = length board
+          cycleLen = boardLen + 3 - 1
+
+          cycledBd = take cycleLen . cycle $ board
+          cycledIndices = 
+            take cycleLen . cycle $ take boardLen . cycle $ iterate ((+) 1) 0
+          cycleListIndexed = zip cycledIndices cycledBd
+
+          possibleTakes = unfoldr getTakes cycleListIndexed
+          -- It's a shitty life we live...
+          validTakes = concatMap (\take -> 
+            nub $ concatMap (\i -> unfoldr (getValidTakes i) take) [1,2,3]) possibleTakes
+      in  unfoldr (nextChildBoard board) validTakes
+
+    spanMinus p ls = (takeWhile p ls, dropWhile (not . p) $ dropWhile p ls)
+
+    getTakes ls
+      | length ls == 0 = Nothing
+      | otherwise      = Just $ spanMinus snd ls
+
+    getValidTakes i ls
+      | length ls == 0 = Nothing
+      | otherwise      = Just (take i ls, drop 1 ls)
+
+    nextChildBoard board toTake
+      | length toTake == 0 = Nothing
+      | otherwise          = Just (appendUpdateWith (toTake !! 0) board, drop 1 toTake)
+
+    appendUpdateWith :: [(Int, Bool)] -> [Bool] -> [Bool]
+    appendUpdateWith updateIndices ls =
+        snd . unzip $
+          map (updateOrLeaveAt updateIndices) (zip (iterate ((+) 1) 0) ls)
+      where updateOrLeaveAt indices (i, e) =
+              case find ((==i) . fst) indices of
+                Nothing      -> (i, e)
+                Just (_, e') -> (i, not e')
+      
+evaluate :: Node -> Int
+evaluate nd =
+  case turn nd of
+    Me -> (-1)
+    Opponent -> 1
 
 
-minMax :: Node -> (Node, Int)
-minMax n =
+-- TODO: Should we use lastEval?
+-- TODO: Add alpha-beta slicing
+minMax :: Node -> Int -> (Node, Int)
+minMax n lastEval =
   case getChildren n of
-    []       -> (n, evaluate n)
-    children -> bestChild children (turn n)
+    []       -> 
+      let eval = evaluate n
+      in  if eval < 0
+          then (n, (-1) * lastEval)
+          else (n, lastEval)
+    children -> bestChild children (turn n) (lastEval+1)
 
-bestChild :: [Node] -> Turn -> (Node, Int)
-bestChild []     _ = error "No children"
-bestChild (x:[]) _ = minMax x
-bestChild (x:xs) t = best (minMax x) (bestChild xs t) t
+bestChild :: [Node] -> Turn -> Int -> (Node, Int)
+bestChild []     _ _        = error "No children"
+bestChild (x:[]) _ lastEval = minMax x lastEval
+bestChild (x:xs) t lastEval = best (minMax x lastEval) (bestChild xs t lastEval) t
 
 best :: (Node, Int) -> (Node, Int) -> Turn -> (Node, Int)
 best (n1, val1) (n2, val2) t
@@ -54,10 +103,19 @@ best (n1, val1) (n2, val2) t
   | otherwise = (n2, val2)
 
 
-getBestMove :: Int -> (Int, Int, Int)
-getBestMove startIdx =
-  let (firstCount, moves) = minMax Node {parent = Nil, board = startIdx, turn = Me}
-  in  (0, board firstCount, moves)
+getBestMove :: [Bool] -> (Int, Int, Int)
+getBestMove startBoard =
+    let (firstCount, moves) = minMax (Node {parent = Nil, board = startBoard, turn = Me}) 0
+    in  (0, getSecondRound firstCount, moves)
+  -- The first move comes from the first child
+  where getSecondRound nd =
+          case parent nd of
+            Nil -> 
+              length . filter (==False) $ board nd
+            p -> 
+              case parent p of
+                Nil -> length . filter (==False) $ board p
+                _   -> getSecondRound p
 
 
 main :: IO ()
@@ -66,13 +124,16 @@ main = do
   case args of
     [coins_count] -> case (validate coins_count) of
                    Left err -> error err
-                   Right count -> printResult $ getBestMove count
+                   Right count -> printResult $ getBestMove (countToBoard count)
     _         -> putStrLn usage
   where printResult (idx, count, moves) =
           putStrLn $ "{ " ++ (show idx) ++ ", " ++ 
                              (show count) ++ ", " ++ 
                              (show moves) ++ " }"
         usage = "Usage: coins <coins_count>"
+
+countToBoard :: Int -> [Bool]
+countToBoard = flip replicate True
 
 validate :: String -> Either String Int
 validate n
