b7564be5db2794ce0232d1f0b1c6bc299faf5910
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2015-12-30 10:25:18 +0200

    Concurrent sieve of Eratosthenes.

---
+++ learning-go/src/github.com/emdeha/goroutine/primes.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+
+func generate(ch chan<- int) {
+	for i := 2; ; i++ {
+		ch <- i
+	}
+}
+
+func filter(src <-chan int, dst chan<- int, prime int) {
+	for i := range src {
+		if i % prime != 0 {
+			dst <- i
+		}
+	}
+}
+
+func sieve() {
+	ch := make(chan int)
+	go generate(ch)
+	for {
+		prime := <-ch
+		fmt.Println(prime)
+		ch1 := make(chan int)
+		go filter(ch, ch1, prime)
+		ch = ch1
+	}
+}
+
+func main() {
+	sieve()
+}
