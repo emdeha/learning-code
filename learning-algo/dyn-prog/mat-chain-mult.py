01a6fec6908a04bc9e10e29e620981acbad08a42
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-01-03 11:10:12 +0200

    Multplication after optimal chaining.

--- learning-algo/dyn-prog/mat-chain-mult.py
+++ learning-algo/dyn-prog/mat-chain-mult.py
@@ -1,4 +1,5 @@
 import sys
+import numpy
 
 
 def matrix_chain_order(p):
@@ -28,7 +29,41 @@ def print_mult(s, i, j):
         print_mult(s, s[i][j] + 1, j)
         sys.stdout.write(")")
 
+def do_prod(A1, A2):
+    print "multiplying"
+
+def mult_matrices(A, s, i, j):
+    if i == j:
+        return A[i]
+    else:
+        return do_prod(mult_matrices(A, s, i, s[i][j]),
+                       mult_matrices(A, s, s[i][j] + 1, j))
+
+
+A1 = [[3, 4, 5, 7, 9, 11],
+      [20, 30, 11, 100, 9],
+      [3, 4, 200, 5, 11, 9]]
+
+A2 = [[1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 1, 1, 1, 1, 2, 1, 2, 1],
+      [1, 1, 2, 1, 1, 3, 1, 1, 1],
+      [1, 2, 3, 1, 2, 3, 1, 2, 3],
+      [2, 3, 4, 2, 3, 4, 2, 3, 4]]
+
+A3 = [[2, 3],
+      [2, 3],
+      [2, 3],
+      [2, 3],
+      [2, 3],
+      [2, 3],
+      [2, 3],
+      [2, 3],
+      [2, 3]]
+
+A = [[], A1, A2, A3]
 
 (m, s) = matrix_chain_order([5,10,3,12,5,50,6])
-print_mult(s, 1, 6)
+prod = mult_matrices(A, s, 1, 3)
+# print_mult(s, 1, 3)
+print "Product: " + str(prod)
 print "\nDone!"
