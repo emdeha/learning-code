311aa16fb0bb10665190126e83be582847aa2634
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-03-07 18:51:35 +0200

    Parsing books.

---
+++ node/05-databases/lib/rdf-parser.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const
+  fs = require('fs'),
+  cheerio = require('cheerio');
+
+module.exports = function(filename, callback) {
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      return callback(err);
+    }
+
+    let
+      $ = cheerio.load(data.toString()),
+      collect = function(index, elem) {
+        return $(elem).text();
+      };
+
+    callback(null, {
+      _id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''), 
+      title: $('dcterms\\:title').text(), 
+      authors: $('pgterms\\:agent pgterms\\:name').map(collect), 
+      subjects: $('dcterms\\:subject rdf\\:value').map(collect) 
+    });
+  });
+};
