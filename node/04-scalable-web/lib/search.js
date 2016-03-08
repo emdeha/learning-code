7c19d094466799b9a78b7f1d53c45cfc39567a05
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-03-08 14:43:56 +0200

    Sanitizes input.

--- node/04-scalable-web/lib/search.js
+++ node/04-scalable-web/lib/search.js
@@ -4,12 +4,19 @@ const request = require('request');
 
 module.exports = function(queryTemplate, config, app, onReceive, qs) {
   app.get(queryTemplate, function(req, res) {
+    if (! /^(author|subject)$/.test(req.params.view)) {
+      res.json(400, {
+        error: "bad_request",
+        reson: "params.view must be 'author' or 'subject'"
+      });
+      return;
+    }
+
     request({
       method: 'GET',
       url: config.bookdb + '_design/books/_view/by_' + req.params.view,
       qs: qs(req)
     }, function(err, couchRes, body) {
-      
       // couldn't connect to CouchDB
       if (err) {
         res.json(502, { error: "bad_gateway", reason: err.code });
