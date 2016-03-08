02bb8f8b1395e1d791831cf3e214a3146ba0b8bc
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-03-08 14:37:34 +0200

    Refactors searches.

---
+++ node/04-scalable-web/lib/search.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const request = require('request');
+
+module.exports = function(queryTemplate, config, app, onReceive, qs) {
+  app.get(queryTemplate, function(req, res) {
+    request({
+      method: 'GET',
+      url: config.bookdb + '_design/books/_view/by_' + req.params.view,
+      qs: qs(req)
+    }, function(err, couchRes, body) {
+      
+      // couldn't connect to CouchDB
+      if (err) {
+        res.json(502, { error: "bad_gateway", reason: err.code });
+        return;
+      }
+      
+      // CouchDB couldn't process our request
+      if (couchRes.statusCode !== 200) {
+        res.json(couchRes.statusCode, JSON.parse(body));
+        return;
+      }
+
+      onReceive(res, body);
+    });
+  });
+};
