de89bfe726c0db158dce7705ac5358f4190d27f6
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-07-06 07:50:09 +0300

    Scripts to use Spark.

---
+++ spark/exec.sh
@@ -0,0 +1,1 @@
+/home/emdeha/Downloads/spark-1.6.2-bin-hadoop2.6/bin/spark-submit --class $1 $2 2>$1.log
---
+++ spark/prepare.sh
@@ -0,0 +1,2 @@
+/home/emdeha/Downloads/spark-1.6.2-bin-hadoop2.6/sbin/start-master.sh
+/home/emdeha/Downloads/spark-1.6.2-bin-hadoop2.6/sbin/start-slave.sh 127.0.1.1:7077
