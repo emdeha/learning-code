23537756b2cbe1f767b387d249768d03dcef2b5f
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-07-06 08:34:10 +0300

    Creates a simple holt data frame.

--- spark/text-proc/src/main/scala/TextProcApp.scala
+++ spark/text-proc/src/main/scala/TextProcApp.scala
@@ -1,17 +1,34 @@
 /* TextProcApp.scala */
 import org.apache.spark._
+import org.apache.spark.rdd._
 import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream._
+import org.apache.spark.sql._
 import scala.util.Try
 
 object TextProcApp {
+  val hwAlpha = 0.26
+  val hwBeta = 0.19
+
+  def holt(data: RDD[Int], alpha: Double, beta: Double): DataFrame = {
+    val sqlCtx = SQLContext.getOrCreate(data.sparkContext)
+    import sqlCtx.implicits._
+
+    val holtDF = data.toDF("numbers")
+    return holtDF
+  }
+
   def main(args: Array[String]) {
     val conf = new SparkConf().setMaster("local[2]").setAppName("TextProcApp")
-    val ssc = new StreamingContext(conf, Seconds(1))
+    val ssc = new StreamingContext(conf, Seconds(10))
 
     val lines = ssc.socketTextStream("localhost", 1337)
     val numbers = lines.flatMap(_.split(" ")).map(n => Try(n.toInt)).filter(_.isSuccess).map(_.get)
+    numbers.foreachRDD { rdd =>
+      val holtDF = holt(rdd, hwAlpha, hwBeta)
 
-    numbers.print()
+      holtDF.show()
+    }
 
     ssc.start()
     ssc.awaitTermination()
