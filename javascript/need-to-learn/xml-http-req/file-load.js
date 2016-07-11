92ff5025e16666ec9d620a6ccbf31b0e09195da1
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-07-11 07:47:49 +0300

    XHR timeouts.

--- javascript/need-to-learn/xml-http-req/file-load.js
+++ javascript/need-to-learn/xml-http-req/file-load.js
@@ -6,13 +6,17 @@ var xhrError = function () {
   console.error(this.statusText)
 }
 
-var loadFile = function (sURL, fCallback /*, args to callback */) {
+var loadFile = function (sURL, timeout, fCallback /*, args to callback */) {
   var oReq = new XMLHttpRequest()
   oReq.callback = fCallback
   oReq.arguments = Array.prototype.slice.call(arguments, 2)
+  oReq.ontimeout = function () {
+    console.error('request timed out')
+  }
   oReq.onload = xhrSuccess
   oReq.onerror = xhrError
-  oReq.open("GET", sURL, true)
+  oReq.open('GET', sURL, true)
+  oReq.timeout = timeout
   oReq.send(null)
 }
 
@@ -20,4 +24,4 @@ var showMsg = function (sMsg) {
   alert(sMsg + this.responseText)
 }
 
-loadFile("https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests", showMsg, "New message\n\n")
+loadFile('https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/Synchronous_and_Asynchronous_Requests', 500, showMsg, 'New message\n\n')
