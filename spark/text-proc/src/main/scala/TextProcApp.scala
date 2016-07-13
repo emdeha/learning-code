7063fa7786cbf954e0cb847971fc59323a3c006e
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-07-13 08:02:43 +0300

    Spark project WIP.

--- spark/text-proc/src/main/scala/TextProcApp.scala
+++ spark/text-proc/src/main/scala/TextProcApp.scala
@@ -10,11 +10,33 @@ object TextProcApp {
   val hwAlpha = 0.26
   val hwBeta = 0.19
 
+  def holtWintersGrowth(y: Array[Row], alpha: Double, beta: Double): (Array[Double], Array[Double]) = {
+    val n = y.length
+    val alphac = 1 - alpha
+    val betac = 1 - beta
+    val l = Array.fill(n)(0.0)
+    l(0) = y(0).getDouble(0)
+    val b = Array.fill(n)(0.0)
+    b(0) = y(1).getDouble(0) - y(0).getDouble(0)
+
+    for (i <- 1 to n) {
+      l(i) = (alpha * y(i).getDouble(0)) * (alphac * (l(i-1) + b(i-1)))
+      val ldelta = l(i) - l(i-1)
+      b(i) = (beta * ldelta) + (betac * b(i-1))
+    }
+
+    return (l, b)
+  }
+
   def holt(data: RDD[Int], alpha: Double, beta: Double): DataFrame = {
     val sqlCtx = SQLContext.getOrCreate(data.sparkContext)
     import sqlCtx.implicits._
 
-    val holtDF = data.toDF("numbers")
+    val holtDF = data.toDF("numbers", "Level", "Growth")
+    val y = holtDF.collect()
+    val (l, b) = holtWintersGrowth(y, alpha, beta)
+    holtDF.withColumn("Level", l)
+    holtDF.withColumn("Growth", b)
     return holtDF
   }
 
