6dc83a28e8ca3c03f58966712688920df432dec4
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-08-17 11:01:40 +0300

    Some changes.

--- ai/mit-6.034/labs/lab1/lab1.py
+++ ai/mit-6.034/labs/lab1/lab1.py
@@ -114,9 +114,69 @@ TEST_RESULTS_TRANS2 = forward_chain([transitive_rule],
 # able to refer to the rules by name and easily rearrange them if
 # you need to.
 
+# x is the same as x
+same = IF (
+          OR ('male (?x)', 'female (?x)'),
+       THEN ('same (?x) (?x)') )
+# x is the sibling of y; y is the sibling of x
+sibling = IF (
+             OR ('brother (?x) (?y)', 'sister (?x) (?y)'),
+          THEN ('sibling (?x) (?y)') )
+# x is the brother of y
+brother = IF (
+             AND ('parent (?x) (?y)', 'parent (?x) (?z)',
+                  'male (?y)', NOT ( 'same (?y) (?z)' )),
+          THEN ('brother (?y) (?z)') )
+# x is the sister of y
+sister = IF (
+             AND ('parent (?x) (?y)', 'parent (?x) (?z)',
+                  'female (?y)', NOT ( 'same (?y) (?z)' )),
+          THEN ('sister (?y) (?z)') )
+# x is the mother of y
+mother = IF (
+            AND ('female (?x)', 'parent (?x) (?y)'),
+         THEN ('mother (?x) (?y)') )
+# x is the father of y
+father = IF (
+            AND ('male (?x)', 'parent (?x) (?y)'),
+         THEN ('father (?x) (?y)') )
+# x is the son of y
+son = IF (
+         AND ('male (?x)', 'parent (?y) (?x)'),
+      THEN ('son (?x) (?y)') )
+# x is the daughter of y
+daughter = IF (
+              AND ('female (?x)', 'parent (?y) (?x)'),
+           THEN ('daughter (?x) (?y)') )
+# x is the cousin of y
+cousin = IF (
+            AND ('parent (?a) (?x)', 'parent (?b) (?y)', 'sibling (?a) (?b)'),
+         THEN ('cousin (?x) (?y)') )
+# x is the grandparent of y
+grandparent = IF (
+                 AND ('parent (?x) (?y)', 'parent (?y) (?z)'),
+              THEN ('grandparent (?x) (?z)') )
+# x is the grandchild of y
+grandchild = IF (
+                AND ('parent (?x) (?y)', 'parent (?y) (?z)'),
+             THEN ('grandchild (?z) (?x)') )
+
+
 # Then, put them together into a list in order, and call it
 # family_rules.
-family_rules = [ ]                    # fill me in
+family_rules = [
+  same,
+  mother,
+  father,
+  son,
+  daughter,
+  grandparent,
+  grandchild,
+  brother,
+  sister,
+  sibling,
+  cousin
+]                    # fill me in
 
 # Some examples to try it on:
 # Note: These are used for testing, so DO NOT CHANGE
