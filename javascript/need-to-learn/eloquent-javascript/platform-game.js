83acc38b03a6ac59c6a23932eba10f98f832a878
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-09-05 09:57:21 +0300

    Updates learning tasks.

--- javascript/need-to-learn/eloquent-javascript/platform-game.js
+++ javascript/need-to-learn/eloquent-javascript/platform-game.js
@@ -236,9 +236,60 @@ function trackKeys (codes) {
       event.preventDefault()
     }
   }
-  addEventListener('keydown', handler)
-  addEventListener('keyup', handler)
+  document.addEventListener('keydown', handler)
+  document.addEventListener('keyup', handler)
   return pressed
+}
+
+/* Running the game */
+function runAnimation (frameFunc) {
+  var lastTime = null
+  var frame = function (time) {
+    var stop = false
+    if (lastTime !== null) {
+      var timeStep = Math.min(time - lastTime, 100) / 1000
+      stop = frameFunc(timeStep) === false
+    }
+    lastTime = time
+    if (!stop) {
+      requestAnimationFrame(frame)
+    }
+  }
+  requestAnimationFrame(frame)
+}
+
+var arrows = trackKeys(arrowCodes)
+
+function runLevel(level, Display, andThen) {
+  var display = new Display(document.body, level)
+  runAnimation(function(step) {
+    level.animate(step, arrows)
+    display.drawFrame(step)
+    if (level.isFinished()) {
+      display.clear()
+      if (andThen) {
+        andThen(level.status)
+      }
+      return false
+    }
+  })
+}
+
+function runGame(plans, Display) {
+  function startLevel(n) {
+    runLevel(new Level(plans[n]), Display, function(status) {
+      if (status == "lost") {
+        startLevel(n)
+      }
+      else if (n < plans.length - 1) {
+        startLevel(n + 1)
+      }
+      else {
+        console.log("You win!")
+      }
+    })
+  }
+  startLevel(0)
 }
 
 
@@ -253,5 +304,4 @@ var simpleLevelPlan = [
   '      xxxxxxxxxxxxxx  ',
   '                      '
 ]
-var simpleLevel = new Level(simpleLevelPlan)
-var Display = new DOMDisplay(document.body, simpleLevel)
+runGame([simpleLevelPlan], DOMDisplay)
