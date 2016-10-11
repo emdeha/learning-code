90aa4a5c6f9685b6ab05f0f5467efe0c09856077
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2016-10-11 15:18:34 +0300

    Implements dynamic server.

--- racket/serve.rkt
+++ racket/serve.rkt
@@ -1,4 +1,5 @@
 #lang racket
+(require xml net/url)
 
 (define (go)
  'yep-it-works)
@@ -28,9 +29,52 @@
           (custodian-shutdown-all cust))))
 
 (define (handle in out)
- ; Discard the request header (up to a blank line):
- (regexp-match #rx"(\r\n|^)\r\n" in)
- ; Send reply:
- (display "HTTP/1.0 200 Okay\r\n" out)
- (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
- (display "<html><body>Hello, world</body></html>" out))
+ (define req
+  ; Extract request
+  (regexp-match #rx"^GET (.+) HTTP/[0-9]+\\.[0-9]+"
+   (read-line in)))
+ (when req
+   ; Discard the request header (up to a blank line):
+   (regexp-match #rx"(\r\n|^)\r\n" in)
+   (let ([xexpr (dispatch (list-ref req 1))])
+     ; Send reply:
+     (display "HTTP/1.0 200 Okay\r\n" out)
+     (display "Server: k\r\nContent-Type: text/html\r\n\r\n" out)
+     (display (xexpr->string xexpr) out))))
+
+(define (dispatch str-path)
+ (define url (string->url str-path))
+ (define path (map path/param-path (url-path url)))
+ (define h (hash-ref dispatch-table (car path) #f))
+ (if h
+  (h (url-query url))
+  `(html (head (title "Error"))
+    (body
+     (font ((color "red"))
+      "Unknown page: ", str-path)))))
+
+(define dispatch-table (make-hash))
+
+(hash-set! dispatch-table "hello"
+ (lambda (query)
+  `(html (body "Hello world!"))))
+
+(define (build-request-page label next-url hidden)
+ `(html
+   (head (title "Enter a number to add"))
+   (body ([bgcolor "white"])
+    (form ([action ,next-url] [method "get"])
+      ,label
+      (input ([type "text"] [name "number"] [value ""]))
+      (input ([type "hidden"] [name "hidden"] [value ,hidden]))
+      (input ([type "submit"] [name "enter"] [value "Enter"]))))))
+
+(define (many query)
+ (build-request-page "Number of greetings:" "/reply" ""))
+
+(define (reply query)
+ (define n (string->number (cdr (assq 'number query))))
+ `(html (body ,@(for/list ([i (in-range n)]) " hello"))))
+
+(hash-set! dispatch-table "many" many)
+(hash-set! dispatch-table "reply" reply)
