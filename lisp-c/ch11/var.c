c4de51a8ea4900a7fde07034953259b333e54d87
Tsvetan Tsvetanov <cpi.cecko@gmail.com>
2017-06-08 18:09:43 +0300

    Implements env structure.

--- lisp-c/ch11/var.c
+++ lisp-c/ch11/var.c
@@ -39,7 +39,7 @@ enum { LVAL_NUM, LVAL_ERR, LVAL_SYM,
 
 typedef lval*(*lbuiltin)(lenv*, lval*);
 
-typedef struct lval {
+struct lval {
   int type;
 
   long num;
@@ -49,7 +49,7 @@ typedef struct lval {
 
   int count;
   struct lval **cell;
-} lval;
+};
 
 lval* lval_num(long x) {
   lval *v = malloc(sizeof(lval));
@@ -137,6 +137,59 @@ lval* lval_copy(lval *v) {
       }
       break;
   }
+
+  return x;
+}
+
+struct lenv {
+  int count;
+  char **syms;
+  lval **vals;
+};
+
+lenv* lenv_new(void) {
+  lenv *e = malloc(sizeof(lenv));
+  e->count = 0;
+  e->syms = NULL;
+  e->vals = NULL;
+  return e;
+}
+
+void lenv_del(lenv *e) {
+  for (int i = 0; i < e->count; i++) {
+    free(e->syms[i]);
+    free(e->vals[i]);
+  }
+  free(e->syms);
+  free(e->vals);
+  free(e);
+}
+
+lval* lenv_get(lenv *e, lval *k) {
+  for (int i = 0; i < e->count; i++) {
+    if (strcmp(e->syms[i], k->sym) == 0) {
+      return lval_copy(e->vals[i]);
+    }
+  }
+  return lval_err("unbound symbol!");
+}
+
+void lval_put(lenv *e, lval *k, lval *v) {
+  for (int i = 0; i < e->count; i++) {
+    if (strcmp(e->syms[i], k->sym) == 0) {
+      lval_del(e->vals[i]);
+      e->vals[i] = lval_copy(v);
+      return;
+    }
+  }
+
+  e->count++;
+  e->vals = realloc(e->vals, sizeof(lval*) * e->count);
+  e->syms = realloc(e->syms, sizeof(char*) * e->count);
+
+  e->vals[e->count-1] = lval_copy(v);
+  e->syms[e->count-1] = malloc(strlen(k->sym) + 1);
+  strcpy(e->syms[e->count-1], k->sym);
 }
 
 lval* lval_read_num(mpc_ast_t *t) {
